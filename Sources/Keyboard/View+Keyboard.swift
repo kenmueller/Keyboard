5162794a2714fb2d1cf614ff3c879b9fb598e848
Ken Mueller <kenmueller0@gmail.com>
2019-11-07 21:28:58 -0800

    public func

--- Sources/Keyboard/View+Keyboard.swift
+++ Sources/Keyboard/View+Keyboard.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-public extension View {
-	func raisesForKeyboard() -> some View {
+extension View {
+	public func raisesForKeyboard() -> some View {
 		KeyboardResponder { self }
 	}
 }
